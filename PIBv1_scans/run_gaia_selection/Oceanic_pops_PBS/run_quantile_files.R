e581c227037a6b5d5196c2080d9e03f523831586
GenomeGaia <60054173+dtejadam@users.noreply.github.com>
2025-09-25 22:03:55 -0400

    Update run_quantile_files.R

--- PIBv1_scans/run_gaia_selection/Oceanic_pops_PBS/run_quantile_files.R
+++ PIBv1_scans/run_gaia_selection/Oceanic_pops_PBS/run_quantile_files.R
@@ -1,5 +1,6 @@
-##############################################
-#     DATA OCEANIA - PBS ALL CHR
+######################################################
+#     PIBv1_manuscript - PBS, get quantiles 
+######################################################
 
 library(qqman)
 library(tidyverse)
@@ -24,7 +25,6 @@ for(i in 1:length(temp)){
 
   write.table(df[df$score>q_0.05,], file = paste0(name, "_0.05.tsv"),
               quote=FALSE, sep="\t", row.names = FALSE)
-
 
   write.table(df[df$score>q_0.01,], file = paste0(name, "_0.01.tsv"),
               quote=FALSE, sep="\t", row.names = FALSE)
